78f072f0ece889b1b3328e5d74e5653186f796bb
Shuang Qiu <sqiuac@connect.ust.hk>
2019-02-24 17:14:23 +0800

    changing to cooperative group

--- Hash/hash.cu
+++ Hash/hash.cu
@@ -22,7 +22,7 @@
 #ifndef HASH_CUH
 #define HASH_CUH
 
-namespace cg = cooperative_groups;
+using namespace cooperative_groups;
 
 template <typename T>
 __device__ inline T insert_int_key (volatile T * key, T new_key)
@@ -346,7 +346,9 @@ __global__ void insert_key_value_hashtab_warp (key_type * input_keys, value_type
 	uint gid = blockDim.x * blockIdx.x + threadIdx.x;
 	uint tid = threadIdx.x;
 	uint warpid = gid / WARP_WIDTH;
-	uint laneid = tid % WARP_WIDTH;
+//	uint laneid = tid % WARP_WIDTH;
+	thread_block_tile<4> tile4 = tiled_partition<4>(this_thread_block());
+	uint laneid = tile4.thread_rank();
 
 	size_t keys_per_warp = (num_of_keys + (MAX_NUM_THREADS/WARP_WIDTH - 1)) / (MAX_NUM_THREADS/WARP_WIDTH); // keys per block
 //	keys_per_warp = (keys_per_warp + WARP_WIDTH - 1) / WARP_WIDTH;
@@ -534,6 +536,189 @@ __global__ void insert_key_value_hashtab_warp (key_type * input_keys, value_type
 	}
 }
 
+__global__ void insert_key_value_hashtab_cooperative_group (key_type * input_keys, value_type * input_values, size_t num_of_keys)
+{
+	uint gid = blockDim.x * blockIdx.x + threadIdx.x;
+	uint tid = threadIdx.x;
+	int warpsize = 8;
+	int slotsize = 8;
+	uint warpid = gid / warpsize;
+	thread_block_tile<4> tile4 = tiled_partition<4>(this_thread_block());
+	uint laneid = tile4.thread_rank();
+	ull full_mask;
+	if (warpsize == 8)
+		full_mask = 0xff000000;
+	else if (warpsize == 16)
+		full_mask = 0xffff0000;
+	else if (warpsize == 32)
+		full_mask = 0xffffffff;
+	else if (warpsize == 4)
+		full_mask = 0xf0000000;
+
+	size_t keys_per_warp = (num_of_keys + (MAX_NUM_THREADS/warpsize - 1)) / (MAX_NUM_THREADS/warpsize); // keys per block
+	uint j;
+	for (j = 0; j < keys_per_warp; j++)
+	{
+		if (warpid * keys_per_warp + j >= num_of_keys)
+			return;
+
+		key_type * key = &(input_keys[warpid * keys_per_warp + j]);
+		value_type * value = &(input_values[warpid * keys_per_warp + j]);
+
+		hashval_t hash = murmur_hash3_32 (key, DEFAULT_SEED);
+		hashval_t index1;
+		void * ptr;
+		state_type * state_ptr;
+		slot_mask_type * slot_mask_ptr;
+		entry_kv_t * entries = htable;
+		entry_kv_t * entry;
+		prime_index_t size_prime_index = d_size_prime_index;
+		hashval_t table_size = dsize;
+		int cont_flag = 0; // continue flag to make warp continue synchronously in a loop
+		bool next_flag = false;
+		uint active;
+		state_type flag = -1;
+
+		index1 = hashtab_mod (hash, size_prime_index); // primary index
+		/* debug only: */
+		if (index1 >= table_size)
+		{
+			continue;
+		}
+
+		//* first try of insertion: *
+		ptr = (void*)((char*)entries + index1 * (sizeof(entry_kv_t) * slotsize + sizeof(slot_mask_type) + sizeof(state_type)));
+		state_ptr = (state_type *)ptr;
+		slot_mask_ptr = (slot_mask_type*)((char*)state_ptr + sizeof(state_type));
+		entry = (entry_kv_t*)((char*)slot_mask_ptr + sizeof(slot_mask_type) + sizeof(entry_kv_t) * laneid);
+
+		while (*slot_mask_ptr != full_mask)
+		{
+			if (laneid == 0)
+			{
+				flag = atomicCAS(state_ptr, 0, 1);
+			}
+			flag = __shfl_sync (full_mask, flag, 0, warpsize); // broadcast flag to threads in this warp
+			active = *slot_mask_ptr;
+			if (flag == 0) // empty slot exists
+			{
+				uint r = MultiplyDeBruijnBitPosition[((uint)(((~active) & -(~active)) * 0x077CB531U)) >> 27];
+				if ((~active) && r == warpsize - 1 - laneid) // thread selected for insertion
+				{
+					insert_int_key<key_type> (&(entry->key), *key);
+					update_slot_mask (slot_mask_ptr, laneid);
+					int ret = atomicCAS (state_ptr, 1, 0);
+					if (ret != 1)
+					{
+						printf ("Error in changing state_flag from locked to occupied!\n");
+					}
+					replace_int_value<value_type> (&(entry->value), *value);
+					cont_flag = 1;
+				}
+				cont_flag = __shfl_sync (full_mask, cont_flag, warpsize-1-r, warpsize);
+				if (cont_flag)
+				{
+					next_flag = true;
+					break;
+				}
+			}
+			if (laneid == 0)
+			{
+				while ((flag = atomicCAS(state_ptr, 0, 0)) != 0) {}
+			}
+			flag = __shfl_sync (full_mask, flag, 0, warpsize); // broadcast flag to threads in this warp
+			int ret = 0;
+			ret = compare_keys (key, &(entry->key));
+			uint ballot_result = __ballot_sync (full_mask, ret);
+			uint r = MultiplyDeBruijnBitPosition[((uint)((ballot_result & -ballot_result) * 0x077CB531U)) >> 27];
+			if (ballot_result && r == warpsize - 1 - laneid)
+			{
+				replace_int_value<value_type> (&(entry->value), *value);
+				cont_flag = 1;
+			}
+			cont_flag = __shfl_sync (full_mask, cont_flag, warpsize-1-r, warpsize);
+			if (cont_flag)
+			{
+				next_flag = true;
+				break;
+			}
+		}
+		if (next_flag)
+			continue;
+
+		//* first try fails, continue with secondary hash2 *
+		hashval_t hash2 = hashtab_mod_m2 (hash, size_prime_index);
+		size_t i;
+		for (i=0; i<table_size; i++)
+		{
+			index1 += hash2;
+			if (index1 >= table_size)
+				index1 -= table_size;
+
+			ptr = (void*)((char*)entries + index1 * (sizeof(entry_kv_t) * warpsize + sizeof(slot_mask_type) + sizeof(state_type)));
+			state_ptr = (state_type *)ptr;
+			slot_mask_ptr = (slot_mask_type*)((char*)state_ptr + sizeof(state_type));
+			entry = (entry_kv_t*)((char*)slot_mask_ptr + sizeof(slot_mask_type) + sizeof(entry_kv_t) * laneid);
+			while (*slot_mask_ptr != full_mask)
+			{
+				if (laneid == 0)
+				{
+					flag = atomicCAS(state_ptr, 0, 1);
+				}
+				flag = __shfl_sync (full_mask, flag, 0, warpsize); // broadcast flag to threads in this warp
+				active = *slot_mask_ptr;
+				if (flag == 0) // empty slot exists
+				{
+					uint r = MultiplyDeBruijnBitPosition[((uint)(((~active) & -(~active)) * 0x077CB531U)) >> 27];
+					if ((~active) && r == warpsize - 1 - laneid) // thread selected for insertion
+					{
+						insert_int_key<key_type> (&(entry->key), *key);
+						update_slot_mask (slot_mask_ptr, laneid);
+						int ret = atomicCAS (state_ptr, 1, 0);
+						if (ret != 1)
+						{
+							printf ("Error in changing state_flag from locked to occupied!\n");
+						}
+						replace_int_value<value_type> (&(entry->value), *value);
+						cont_flag = 1;
+					}
+					cont_flag = __shfl_sync (full_mask, cont_flag, warpsize-1-r, warpsize);
+					if (cont_flag)
+					{
+						next_flag = true;
+						break;
+					}
+				}
+				if (laneid == 0)
+				{
+					while ((flag = atomicCAS(state_ptr, 0, 0)) != 0) {}
+				}
+				flag = __shfl_sync (full_mask, flag, 0, warpsize); // broadcast flag to threads in this warp
+				int ret = 0;
+				ret = compare_keys (key, &(entry->key));
+				uint ballot_result = __ballot_sync (full_mask, ret);
+				uint r = MultiplyDeBruijnBitPosition[((uint)((ballot_result & -ballot_result) * 0x077CB531U)) >> 27];
+				if (ballot_result && r == warpsize - 1 - laneid)
+				{
+					replace_int_value<value_type> (&(entry->value), *value);
+					cont_flag = 1;
+				}
+				cont_flag = __shfl_sync (full_mask, cont_flag, warpsize-1-r, warpsize);
+				if (cont_flag)
+				{
+					next_flag = true;
+					break;
+				}
+			}
+			if (next_flag)
+				break;
+		}
+		if (i==table_size && next_flag==false)
+			printf ("error! space not found!!!\n");
+	}
+}
+
+
 void TestDriver::construct_hashtab_gpu (key_type * keys, value_type * values, size_t num_of_keys)
 {
 	HashTab htab;
@@ -588,7 +773,8 @@ void TestDriver::construct_hashtab_warp_gpu (key_type * keys, value_type * value
 	CUDA_CHECK_RETURN (cudaDeviceSynchronize());
 	gettimeofday (&start, NULL);
 	htab.createHashtab(num_of_keys, device_type::gpu);
-	insert_key_value_hashtab_warp<<<MAX_NUM_BLOCKS, THREADS_PER_BLOCK>>>(dkeys, dvalues, num_of_keys);
+//	insert_key_value_hashtab_warp<<<MAX_NUM_BLOCKS, THREADS_PER_BLOCK>>>(dkeys, dvalues, num_of_keys);
+	insert_key_value_hashtab_cooperative_group<<<MAX_NUM_BLOCKS, THREADS_PER_BLOCK>>>(dkeys, dvalues, num_of_keys);
 	CUDA_CHECK_RETURN (cudaDeviceSynchronize());
 	gettimeofday (&end, NULL);
 	hashInsertTime = (float)((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000;
